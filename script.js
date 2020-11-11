188b2bf7e07c59d85e16356c33c80e83e09a7465
Jay Valdez <jj.valdez09@gmail.com>
2020-11-10 19:38:03 -0600

    minor changes

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ var searchButton = document.querySelector("#search-button");
 var searchSong = document.getElementById("searchsong")
 var recentSongTitle = document.getElementById("recentsongtitle")
 
+fetch("https://genius.p.rapidapi.com/search?q=%20", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "a91bea8515msh1cd8f24e11a9b85p19f2a0jsn54911ff43d9f",
+		"x-rapidapi-host": "genius.p.rapidapi.com"
+	}
+})
+.then(response => response.json())
+.then(response => {
+	console.log(response)
+.catch(err => {
+	console.error(err);
+});
+
+
+
+
 
 // Adding an event listener for the search button
 searchButton.addEventListener("click", function(event){
