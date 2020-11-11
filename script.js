5e361ca09aece8ea3ad6e63d0069dad987badd3c
Eduardo Ibarra <eddieibarra43@gmail.com>
2020-11-10 18:44:39 -0600

    reading

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Declaring variables for code. 
 
 var searchBar = document.querySelector("#search-bar");
-var searchButton = document.querySelector("#search-button");
-var searchSong = document.getElementById("searchsong")
-var recentSongTitle = document.getElementById("recentsongtitle")
+var searchButton = document.querySelector(".search-button");
+// var searchSong = document.getElementById("searchsong")
+// var recentSongTitle = document.getElementById("recentsongtitle")
 
 
 // Adding an event listener for the search button
@@ -16,19 +16,19 @@ searchButton.addEventListener("click", function(event){
      
     console.log(userInput);
 
-    var queryURL = "";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        .then(function(response) {
+    // var queryURL = "";
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   })
+    //     .then(function(response) {
             
-            console.log(response);
-        });
+    //         console.log(response);
+    //     });
 
 
    
-    searchBar.value = "";
+    // searchBar.value = "";
     
     
     })
